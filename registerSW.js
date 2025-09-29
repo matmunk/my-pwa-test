20719b7dff201758d39428d43ea1fea1ae7a4541
Mathias Munk Hansen <hiha@ms.nrgisystems.dk>
2025-09-29 11:25:36 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/my-pwa-test/sw.js', { scope: '/my-pwa-test/' })})}
